1758ecaaaeb6daf34959c390faaf23eff0a0ab75
Peterson <petersonsimiao1@gmail.com>
2021-02-25 23:33:29 -0300

    feat: Criado estado de error para o componente input

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -1,31 +1,41 @@
-import { InputHTMLAttributes, useEffect, useRef } from 'react';
-import {IconBaseProps} from 'react-icons'
-import { useField} from '@unform/core';
-import * as S from './style';
+import { InputHTMLAttributes, useEffect, useRef } from "react";
+import { IconBaseProps } from "react-icons";
+import { useField } from "@unform/core";
+import * as S from "./style";
+import { FiAlertCircle } from "react-icons/fi";
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  name: string,
-  icon?: React.ComponentType<IconBaseProps>
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon:Icon ,  ...props}) => {
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...props }) => {
   const inputRef = useRef(null);
   const { fieldName, defaultValue, error, registerField } = useField(name);
 
-    useEffect(() => {
-      registerField({
-        name: fieldName,
-        ref: inputRef.current,
-        path: 'value'
-        })
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value",
+    });
   }, [fieldName, registerField]);
 
   return (
-    <S.Container> 
-    <S.Input defaultValue={defaultValue} ref={inputRef} {...props}/> 
-    { Icon && <Icon/> }
-    {error}
-  </S.Container>
-    )
-  }
+    <S.Container>
+      <S.Input
+        isError={!!error}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...props}
+      />
+      {Icon && <Icon />}
+      {error && (
+        <S.Error message={error}>
+          <FiAlertCircle color="#c53030" size={16} />
+        </S.Error>
+      )}
+    </S.Container>
+  );
+};
 
-export default Input
+export default Input;
--- src/components/Inputs/style.ts
+++ src/components/Inputs/style.ts
@@ -1,4 +1,8 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import Tooltip from "../Tooltip/";
+interface InputProps {
+  isError?: boolean;
+}
 
 export const Container = styled.div`
   position: relative;
@@ -7,17 +11,17 @@ export const Container = styled.div`
     margin-bottom: 14px;
   }
 
-  svg {
+  > svg {
     color: #666360;
     left: 16px;
     pointer-events: none;
     position: absolute;
-    top: 50%;
+    top: 28px;
     transform: translateY(-50%);
   }
 `;
 
-export const Input = styled.input`
+export const Input = styled.input<InputProps>`
   background: #231229;
   border: 2px solid #231229;
   border-radius: 10px;
@@ -43,5 +47,33 @@ export const Input = styled.input`
   &:not(:placeholder-shown) ~ svg,
   &:focus ~ svg {
     color: #ff9000;
+  }
+
+  ${({ isError }) =>
+    isError &&
+    css`
+      border-color: #c53030;
+      padding-right: 38px;
+    `}
+`;
+
+export const Error = styled(Tooltip)`
+  align-items: center;
+  color: #666360;
+  display: flex;
+  justify-content: center;
+  right: 16px;
+  position: absolute;
+  top: 28px;
+  transform: translateY(-50%);
+
+  span {
+    background-color: #c53030;
+    border-color: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
